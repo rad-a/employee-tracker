d736cd97eea6d43d8190c49352b31ba012c72591
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-21 08:36:55 -0500

    Added database schemas

--- db/employeeTrackerSchema.sql
+++ db/employeeTrackerSchema.sql
@@ -3,28 +3,39 @@ CREATE DATABASE employees_DB;
 
 USE employees_DB;
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT, 
-    `name` VARCHAR (30) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE department
+(
+    id INT NOT NULL
+    AUTO_INCREMENT, 
+    `name` VARCHAR
+    (30) NOT NULL,
+    PRIMARY KEY
+    (id)
 );
 
-CREATE TABLE role (
+    CREATE TABLE `role`
+    (
     id INT NOT NULL AUTO_INCREMENT, 
-    title VARCHAR (30) NOT NULL,
-    salary DECIMAL (10,2) NOT NULL,
-    department_id INT NOT NULL, 
-    PRIMARY KEY (id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department (id)
+    title VARCHAR
+    (30) NOT NULL,
+    salary DECIMAL
+    (10,2) NOT NULL,
+    department_id INT, 
+    PRIMARY KEY
+    (id)
+    -- CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT, 
-    first_name VARCHAR (30) NOT NULL,
-    last_name VARCHAR (30) NOT NULL,
+    CREATE TABLE employee
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT, 
+    first_name VARCHAR
+        (30) NOT NULL,
+    last_name VARCHAR
+        (30) NOT NULL,
     role_id INT NOT NULL, 
-    manager_id NULL, 
-    PRIMARY KEY (id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id)
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+    manager_id INT NULL, 
+    PRIMARY KEY
+        (id)
 );
