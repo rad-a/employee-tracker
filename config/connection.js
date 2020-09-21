ff3ce8cc03bf0d8b052539fbff864db4b483322c
rad-a <11688089+rad-a@users.noreply.github.com>
2020-09-21 08:36:11 -0500

    Set up connection to database

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,24 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 require("dotenv").config();
 
 const db_pw = process.env.DB_PASSWORD;
+const db_name = process.env.DB_NAME;
 
 // create the connection information for the sql database
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
-  user: 'root',
-  password: db_pw || '',
-  database: 'employees_DB'
+  user: "root",
+  password: db_pw || "",
+  database: db_name,
 });
 
 // connect to the mysql server and sql database
 connection.connect((err) => {
-  if (err) throw err;
+  if (err) console.log(err);
 
   console.log(`Connected to the database. ID: ${connection.threadId}`);
-//   start();
 });
-
 
 module.exports = connection;
